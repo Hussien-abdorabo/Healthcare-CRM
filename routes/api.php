b3bcaee51e8c282e4e34e9616fc66161c6ad37e6
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-02-23 23:06:14 +0200

    login and logout endpoints

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,12 @@ use Illuminate\Http\Request;
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login',[AuthController::class, 'login']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 
 });
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+
