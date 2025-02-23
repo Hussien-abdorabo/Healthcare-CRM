d0e76d3b746576a248f0f84fc9a914c53eef0d4b
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-02-24 00:35:27 +0200

    book an appointment

--- app/Http/Controllers/Api/AppointmentController.php
+++ app/Http/Controllers/Api/AppointmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
@@ -27,7 +28,22 @@ class AppointmentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $user = auth()->user();
+
+        $validated = $request->validate([
+           'doctor_id'=> 'required',
+           'appointment_date'=> 'required | date|after:today',
+        ]);
+
+        $appointment = Appointment::create([
+            'patient_id' => auth()->id(),
+           'doctor_id' => $validated['doctor_id'],
+           'appointment_date' => $validated['appointment_date'],
+        ]);
+        return response()->json([
+            "message" => "Appointment booked successfully",
+            'data' => $appointment
+        ]);
     }
 
     /**
