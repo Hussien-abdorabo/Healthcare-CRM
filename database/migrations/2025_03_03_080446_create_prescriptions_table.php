8dd2aed122ca8e8e99ec750a7736592288b6375b
Hussien Abdorabo <hussienabdorabbo@gmail.com>
2025-03-03 10:17:26 +0200

    Create 2025_03_03_080446_create_prescriptions_table.php
    create migration to prescription

---
+++ database/migrations/2025_03_03_080446_create_prescriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('medical_record_id')->constrained('medical_records')->cascadeOnDelete();
+            $table->foreignId('patient_id')->constrained('users')->cascadeOnDelete();
+            $table->foreignId('doctor_id')->constrained('users')->cascadeOnDelete();
+
+            $table->text('medications');
+            $table->string('notes')->nullable();
+            $table->boolean('sent_via_whatsapp')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+};
